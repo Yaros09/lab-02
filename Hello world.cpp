77dc9afe89d31f184c388f0023c3c5efdf3b7ccf
Yaros09 <yaroslavdomov@gmail.com>
2022-03-15 14:58:23 +0300

    Changed cpp file

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 int main(int argc, char** argv){
-cout << "Hello world" << endl;
+	string name;
+	cin>>name;
+	cout << "Hello world from " << name << endl;
 }
