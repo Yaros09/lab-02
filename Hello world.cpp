2424b5587a0d7d3316f894656a4f92662f7c43fd
Yaros09 <91633974+Yaros09@users.noreply.github.com>
2022-03-28 22:27:36 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char** argv){
-	string name; \\User name
-	std::cin>>name; \\Input user name
+	string name; \\User name.
+	std::cin>>name; \\Введите имя
 	std::cout << "Hello world from " << name <<std::endl;
 }
